e7f8a007301997c5c08fc7a2ba5f5c768fe941c8
yayayat <yayayat24@gmail.com>
2020-07-25 15:40:55 +0300

    Протестировал nrfStatus

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ int main(void){
     sysInit();
     xprintf("initialization...\n");
     while(1){
-        xprintf("%02X\n",nrfR(NOP));
-        for(volatile uint32_t i=0;i<1000000;i++);
+        xprintf("%02X\n",nrfStatus());
+        _delay_us(1000000);
     }
 }
